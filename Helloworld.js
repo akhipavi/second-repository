6014052b792990755958c359c2b9cf3e2bfb4fda
akhipavi <61534351+akhipavi@users.noreply.github.com>
2020-03-02 11:41:43 +0530

    Update Helloworld.js

--- Helloworld.js
+++ Helloworld.js
@@ -1,3 +1,3 @@
 for (i=0;i<10;i++) {
-    console.log("   Hello world ");
+    console.log("   this is the one which im trying to edit ur code without having access to that... but there is soemthing called fork which comes to action now but i dont know about it  ");
 }
