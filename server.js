139fb0bae15314773f97d11cf6f3433e89142a94
SrinidhiMaddala <53556602+SrinidhiMaddala@users.noreply.github.com>
2020-03-02 11:37:54 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,8 @@
+var http = require('http');
+http.createServer(function(request,response){
+
+    response.writeHead(200,{'Content-Type':'text/html'});
+    response.write("Hello world");
+    response.end();
+}).listen(8080);
+console.log("nidhi");
